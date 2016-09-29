b61c5ed73baab250ebbb9817cc6a48ac4f2eaf69
Leone Parise <leone.parise@gmail.com>
2016-09-29 14:17:30 -0300

    Initial Commit

---
+++ GoogleMaps/GoogleMaps.h
@@ -0,0 +1,19 @@
+//
+//  GoogleMaps.h
+//  GoogleMaps
+//
+//  Created by Leone Parise Vieira da Silva on 29/09/16.
+//  Copyright © 2016 com.google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GoogleMaps.
+FOUNDATION_EXPORT double GoogleMapsVersionNumber;
+
+//! Project version string for GoogleMaps.
+FOUNDATION_EXPORT const unsigned char GoogleMapsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleMaps/PublicHeader.h>
+
+
