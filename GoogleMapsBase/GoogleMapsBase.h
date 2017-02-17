1a0383630bd70fb01c998d3d171939d89a0af148
Ian Kazlauskas <nayzak90@gmail.com>
2017-02-17 18:11:47 +0900

    updated GoogleMaps to 2.2.0

--- GoogleMapsBase/GoogleMapsBase.h
+++ GoogleMapsBase/GoogleMapsBase.h
@@ -1,20 +1,2 @@
-//
-//  GoogleMapsBase.h
-//  GoogleMapsBase
-//
-//  Created by Leone Parise Vieira da Silva on 29/09/16.
-//  Copyright © 2016 com.google. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
-
-//! Project version number for GoogleMapsBase.
-FOUNDATION_EXPORT double GoogleMapsBaseVersionNumber;
-
-//! Project version string for GoogleMapsBase.
-FOUNDATION_EXPORT const unsigned char GoogleMapsBaseVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <GoogleMapsBase/PublicHeader.h>
-
 #import <GoogleMapsBase/GMSCompatabilityMacros.h>
 #import <GoogleMapsBase/GMSCoordinateBounds.h>
